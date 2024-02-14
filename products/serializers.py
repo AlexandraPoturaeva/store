5dd18df2b50aa8f9406e02f414596b15939225fa
AleksandraPoturaeva <av-poturaeva@yandex.ru>
2024-02-14 22:30:16 +0300

    create serializers, views and endpoints for managing shopping cart, add description to each view in products app

--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from versatileimagefield.serializers import VersatileImageFieldSerializer
-from .models import Product, Category, SubCategory
+from .models import Product, Category, SubCategory, ShoppingCart, ProductInCart
 
 
 class SubCategorySerializer(serializers.ModelSerializer):
@@ -34,6 +34,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
+            'id',
             'title',
             'slug',
             'category',
@@ -41,3 +42,61 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'cover',
         ]
+
+
+class ProductInCartSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    title = serializers.SerializerMethodField()
+    price_for_one = serializers.SerializerMethodField()
+    quantity = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ProductInCart
+        fields = ['id', 'title', 'quantity', 'price_for_one']
+
+    def get_id(self, obj):
+        return obj.product.id
+
+    def get_title(self, obj):
+        return obj.product.title
+
+    def get_price_for_one(self, obj):
+        return obj.product.price
+
+    def get_quantity(self, obj):
+        return obj.quantity
+
+
+class ShoppingCartSerializer(serializers.ModelSerializer):
+    products = serializers.SerializerMethodField()
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ShoppingCart
+        fields = ['products', 'total_price']
+
+    def get_products(self, obj):
+        products = ProductInCart.objects.filter(
+            cart=obj,
+        ).prefetch_related('product')
+        return [ProductInCartSerializer(product).data for product in products]
+
+    def get_total_price(self, obj):
+        products = ProductInCart.objects.filter(
+            cart=obj,
+        ).prefetch_related('product')
+
+        total_price = sum([
+            product.product.price * product.quantity
+            for product in products
+        ])
+        return total_price
+
+
+class UpdateProductInCartSerializer(serializers.Serializer):
+    id = serializers.IntegerField(min_value=1)
+    quantity = serializers.IntegerField(min_value=0)
+
+
+class ManageShoppingCartSerializer(serializers.Serializer):
+    products = UpdateProductInCartSerializer(many=True)
